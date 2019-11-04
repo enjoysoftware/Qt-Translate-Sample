373b2e8ae5cb82c55160ab35219ff918ca7eb8d1
enjoysoftware3 <enjoysoftware3@gmail.com>
2019-11-04 10:19:11 +0900

    翻訳ファイルをリソース化

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@
 #include <QLocale>
 int main(int argc, char *argv[])
 {
-    QApplication::setStyle("fusion");
+//    QApplication::setStyle("fusion");
     QApplication a(argc, argv);
     // translate --->
       QTranslator translator;
-      translator.load("translate/trans_"+QLocale::system().name());
+      translator.load("trans_"+QLocale::system().name(),":translate");
       a.installTranslator(&translator);
       // <---
     MainWindow w;
