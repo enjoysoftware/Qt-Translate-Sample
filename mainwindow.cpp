89249971e3c34602e811dde12550639ba63df398
sunboy3 <example@example.com>
2019-11-04 10:40:15 +0900

    localeを表示するよう変更

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -24,8 +24,13 @@ void MainWindow::on_pushButton_2_clicked()
     std::string hello1 = hello.toUtf8().constData();//Qstringからstd::stringへ
     std::cout << hello1;
 }
-
 void MainWindow::on_actionAbout_Qt_triggered()
 {
         QMessageBox::aboutQt(this);
+}
+void MainWindow::slot1(){
+    QMessageBox::warning(this,tr("Warning"),tr("This is a new message."));
+}
+void MainWindow::kyozetu(){
+    QMessageBox::information(this,tr("Rejected"),tr("Rejected."));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -20,9 +20,9 @@ public:
 
 private slots:
     void on_pushButton_clicked();
-
     void on_pushButton_2_clicked();
-
+    void slot1();
+    void kyozetu();
     void on_actionAbout_Qt_triggered();
 
 private:
